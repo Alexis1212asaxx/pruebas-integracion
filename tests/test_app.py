fb8249887dcc7248b053aa22f2cb76fe3fd1bcf5
Alexis1212asaxx <losprimosjuniors50@gmail.com>
2025-10-28 00:59:43 -0600

    cambios en app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,9 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../'))  # Buscar en la carpeta raíz
+# Agregar carpeta raíz al path
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../'))
 
-from app import app, productos, carrito
+from app.app import app, productos, carrito
 import pytest
 
 @pytest.fixture
@@ -12,12 +13,14 @@ def client():
         yield client
 
 def test_index(client):
+    # Verifica que la página principal cargue y muestre productos
     rv = client.get('/')
     assert rv.status_code == 200
     for producto in productos:
         assert bytes(producto['nombre'], 'utf-8') in rv.data
 
 def test_agregar_carrito(client):
+    # Verifica que se agregue un producto al carrito
     carrito.clear()
     rv = client.get('/agregar/1', follow_redirects=True)
     assert rv.status_code == 200
@@ -25,6 +28,7 @@ def test_agregar_carrito(client):
     assert carrito[0]['id'] == 1
 
 def test_ver_carrito(client):
+    # Verifica que el carrito muestre los productos
     carrito.clear()
     carrito.append(productos[0])
     rv = client.get('/carrito')
